750a593f1913dfb3e46a7b9d090e8aea74bfb888
JuliettAlex <juliettalex@yandex.ru>
2013-03-31 19:53:12 +0600

    Initial

---
+++ ImageClientWPF/ImageClientWPF/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+
+namespace ImageClientWPF
+{
+    public partial class MainWindow
+    {
+        const int BUFFER_SIZE = 1024;
+        const int PORT = 5676;
+        IPAddress IP_ADDRESS = new IPAddress(new byte[] { 127, 0, 0, 1 });
+
+        ObservableCollection<BitmapImage> items = new ObservableCollection<BitmapImage>();
+
+        private void ClientStart()
+        {
+            using (TcpClient client = new TcpClient())
+            {
+                client.Connect(IP_ADDRESS, PORT);
+
+                using (NetworkStream networkStream = client.GetStream())
+                {
+                    while (true)
+                    {
+                        if (networkStream.DataAvailable)
+                        {
+                            byte[] length = new byte[4];
+                            int bytesRead = networkStream.Read(length, 0, 4);
+                            int dataLength = BitConverter.ToInt32(length, 0);
+                            int allBytesRead = 0;
+
+                            int bytesLeft = dataLength;
+                            byte[] data = new byte[dataLength];
+
+                            while (bytesLeft > 0)
+                            {
+                                int nextPacketSize = (bytesLeft > BUFFER_SIZE) ? BUFFER_SIZE : bytesLeft;
+                                bytesRead = networkStream.Read(data, allBytesRead, nextPacketSize);
+                                allBytesRead += bytesRead;
+                                bytesLeft -= bytesRead;
+                            }
+
+                            Application.Current.Dispatcher.BeginInvoke(
+                            DispatcherPriority.Background,
+                                new Action(() =>
+                                    {
+                                        using (MemoryStream memoryStream = new MemoryStream(data))
+                                        {
+                                            memoryStream.Position = 0;
+                                            BitmapImage bitmapImage = new BitmapImage();
+                                            bitmapImage.BeginInit();
+                                            bitmapImage.StreamSource = memoryStream;
+                                            bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                                            bitmapImage.DecodePixelHeight = 250;
+                                            bitmapImage.EndInit();
+                                            items.Add(bitmapImage);
+                                        }
+                                    }));
+                        }
+                    }
+                }
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            listbox1.ItemsSource = items;
+            Thread thread = new Thread(ClientStart);
+            thread.IsBackground = true;
+            thread.Start();
+        }
+    }
+}
---
+++ ImageServer/ImageServer/Program.cs
@@ -0,0 +1,108 @@
+﻿
+using System.Drawing;
+using System.Drawing.Imaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Threading;
+
+
+namespace ImageServer
+{
+    class Program
+    {
+        static int countClient = 0;
+        const int PORT = 5676;
+        IPAddress IP_ADDRESS = new IPAddress(new byte[] { 127, 0, 0, 1 });
+        const int BUFFER_SIZE = 1024;
+        const int DATA_LENGTH_OFFSET = 4;
+
+        private void ServerStart()
+        {
+            TcpListener listener = new TcpListener(IP_ADDRESS, PORT);
+            listener.Start();
+            Console.WriteLine("Server running, listening to port " + PORT + " at " + IP_ADDRESS);
+
+            Task parentTask = Task.Factory.StartNew(() =>
+            {
+                while (true)
+                {
+                    TcpClient client = listener.AcceptTcpClient();
+                    Task.Factory.StartNew(() => Service(client), TaskCreationOptions.LongRunning);
+                }
+            }, TaskCreationOptions.LongRunning);
+
+            parentTask.Wait();
+            listener.Stop();
+
+        }
+
+        private void Service(TcpClient tcpClient)
+        {
+            try
+            {
+                countClient++;
+
+                using (NetworkStream networkStream = tcpClient.GetStream())
+                {
+                    Console.WriteLine("Клиентов " + countClient + " подключено");
+
+                    foreach (FileInfo f in GetFilesFromDir())
+                    {
+                        byte[] data = File.ReadAllBytes(f.FullName);
+                        byte[] dataLength = BitConverter.GetBytes(data.Length);
+                        byte[] package = new byte[DATA_LENGTH_OFFSET + data.Length];
+                        dataLength.CopyTo(package, 0);
+                        data.CopyTo(package, DATA_LENGTH_OFFSET);
+
+                        int bytesSent = 0;
+                        int bytesLeft = package.Length;
+
+                        while (bytesLeft > 0)
+                        {
+                            int nextPacketSize = (bytesLeft > BUFFER_SIZE) ? BUFFER_SIZE : bytesLeft;
+                            networkStream.Write(package, bytesSent, nextPacketSize);
+                            bytesSent += nextPacketSize;
+                            bytesLeft -= nextPacketSize;
+                        }
+                        Thread.Sleep(500);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Клиентов " + (countClient - 1) + " подключено");
+                countClient--;
+            }
+        }
+
+        private FileInfo[] GetFilesFromDir()
+        {
+            DirectoryInfo dir = new DirectoryInfo("photos");
+            FileInfo[] files = dir.GetFiles();
+            return files;
+        }
+
+        static void Main(string[] args)
+        {
+            bool onlyInstance = false;
+            Mutex mutex = new Mutex(true, "ImageServer", out onlyInstance);
+            if (!onlyInstance)
+            {
+                return;
+            }
+            Program server = new Program();
+            server.ServerStart();
+        }
+    }
+}
+
